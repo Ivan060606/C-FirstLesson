d03ea912ddb7bfce107b3976af27de446d983c16
Ivan Maltsev <imaltsev2018i@gmail.com>
2022-08-24 15:59:06 +0300

    first lesson, first task

---
+++ Example1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("ВВедите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("ВВедите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a-b>0) Console.WriteLine("Max="+a);
+else if(b-a>0) Console.WriteLine("Max="+b);
+else Console.WriteLine("Числа равны");
